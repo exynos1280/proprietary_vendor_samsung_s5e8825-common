baee8ba2a4d261730a7581c052499c6692330dfb
Ksawlii <ksawery.blaszczak@proton.me>
2025-06-22 23:03:18 +0200

    a53x-common: add missing firmware bins
    Signed-off-by: Ksawlii <ksawery.blaszczak@proton.me>

--- a53x-common-vendor.mk
+++ a53x-common-vendor.mk
@@ -38,6 +38,7 @@ PRODUCT_COPY_FILES += \
     vendor/samsung/a53x-common/proprietary/vendor/firmware/APDV_AUDIO_SLSI.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/APDV_AUDIO_SLSI.bin \
     vendor/samsung/a53x-common/proprietary/vendor/firmware/APSV_AUDIO_SLSI.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/APSV_AUDIO_SLSI.bin \
     vendor/samsung/a53x-common/proprietary/vendor/firmware/AP_AUDIO_SLSI.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/AP_AUDIO_SLSI.bin \
+    vendor/samsung/a53x-common/proprietary/vendor/firmware/NPU.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/NPU.bin \
     vendor/samsung/a53x-common/proprietary/vendor/firmware/OD_V2.1.6_01_26_QVGA.nnc:$(TARGET_COPY_OUT_VENDOR)/firmware/OD_V2.1.6_01_26_QVGA.nnc \
     vendor/samsung/a53x-common/proprietary/vendor/firmware/OD_V2.3.7_02_05_VGA.nnc:$(TARGET_COPY_OUT_VENDOR)/firmware/OD_V2.3.7_02_05_VGA.nnc \
     vendor/samsung/a53x-common/proprietary/vendor/firmware/SetMultiCalInfo.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/SetMultiCalInfo.bin \
@@ -48,28 +49,48 @@ PRODUCT_COPY_FILES += \
     vendor/samsung/a53x-common/proprietary/vendor/firmware/avsyncmix.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/avsyncmix.bin \
     vendor/samsung/a53x-common/proprietary/vendor/firmware/bidirmic.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/bidirmic.bin \
     vendor/samsung/a53x-common/proprietary/vendor/firmware/calliope_dram.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/calliope_dram.bin \
+    vendor/samsung/a53x-common/proprietary/vendor/firmware/calliope_sram.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/calliope_sram.bin \
     vendor/samsung/a53x-common/proprietary/vendor/firmware/dax_param.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/dax_param.bin \
     vendor/samsung/a53x-common/proprietary/vendor/firmware/default_configuration.flm.cfg.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/default_configuration.flm.cfg.bin \
     vendor/samsung/a53x-common/proprietary/vendor/firmware/dolby.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/dolby.bin \
+    vendor/samsung/a53x-common/proprietary/vendor/firmware/dual_cal_wide_sub.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/dual_cal_wide_sub.bin \
     vendor/samsung/a53x-common/proprietary/vendor/firmware/gt9895_a53x.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/gt9895_a53x.bin \
+    vendor/samsung/a53x-common/proprietary/vendor/firmware/is_fw_mcu.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/is_fw_mcu.bin \
     vendor/samsung/a53x-common/proprietary/vendor/firmware/is_lib.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/is_lib.bin \
     vendor/samsung/a53x-common/proprietary/vendor/firmware/is_rta.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/is_rta.bin \
     vendor/samsung/a53x-common/proprietary/vendor/firmware/listenbackeffect.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/listenbackeffect.bin \
     vendor/samsung/a53x-common/proprietary/vendor/firmware/lmecpu_GC5035.json:$(TARGET_COPY_OUT_VENDOR)/firmware/lmecpu_GC5035.json \
+    vendor/samsung/a53x-common/proprietary/vendor/firmware/lmecpu_HI1336.json:$(TARGET_COPY_OUT_VENDOR)/firmware/lmecpu_HI1336.json \
+    vendor/samsung/a53x-common/proprietary/vendor/firmware/lmecpu_IMX258.json:$(TARGET_COPY_OUT_VENDOR)/firmware/lmecpu_IMX258.json \
+    vendor/samsung/a53x-common/proprietary/vendor/firmware/lmecpu_IMX616.json:$(TARGET_COPY_OUT_VENDOR)/firmware/lmecpu_IMX616.json \
+    vendor/samsung/a53x-common/proprietary/vendor/firmware/lmecpu_IMX682.json:$(TARGET_COPY_OUT_VENDOR)/firmware/lmecpu_IMX682.json \
     vendor/samsung/a53x-common/proprietary/vendor/firmware/lmecpu_S5KGD2.json:$(TARGET_COPY_OUT_VENDOR)/firmware/lmecpu_S5KGD2.json \
+    vendor/samsung/a53x-common/proprietary/vendor/firmware/lmecpu_S5KGW1.json:$(TARGET_COPY_OUT_VENDOR)/firmware/lmecpu_S5KGW1.json \
+    vendor/samsung/a53x-common/proprietary/vendor/firmware/mfc_fw.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/mfc_fw.bin \
+    vendor/samsung/a53x-common/proprietary/vendor/firmware/nfc/sec_s3nrn4v_firmware.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/nfc/sec_s3nrn4v_firmware.bin \
+    vendor/samsung/a53x-common/proprietary/vendor/firmware/os.checked.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/os.checked.bin \
     vendor/samsung/a53x-common/proprietary/vendor/firmware/rxse.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/rxse.bin \
     vendor/samsung/a53x-common/proprietary/vendor/firmware/sb_music.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/sb_music.bin \
     vendor/samsung/a53x-common/proprietary/vendor/firmware/sb_others.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/sb_others.bin \
     vendor/samsung/a53x-common/proprietary/vendor/firmware/sectiongraph_tplg.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/sectiongraph_tplg.bin \
     vendor/samsung/a53x-common/proprietary/vendor/firmware/sectiongraph_tplg.conf:$(TARGET_COPY_OUT_VENDOR)/firmware/sectiongraph_tplg.conf \
     vendor/samsung/a53x-common/proprietary/vendor/firmware/secure_configuration.flm.cfg.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/secure_configuration.flm.cfg.bin \
+    vendor/samsung/a53x-common/proprietary/vendor/firmware/setfile_gc5035d.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/setfile_gc5035d.bin \
     vendor/samsung/a53x-common/proprietary/vendor/firmware/setfile_gc5035m.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/setfile_gc5035m.bin \
+    vendor/samsung/a53x-common/proprietary/vendor/firmware/setfile_gd2.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/setfile_gd2.bin \
     vendor/samsung/a53x-common/proprietary/vendor/firmware/setfile_gw1.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/setfile_gw1.bin \
+    vendor/samsung/a53x-common/proprietary/vendor/firmware/setfile_hi1336.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/setfile_hi1336.bin \
+    vendor/samsung/a53x-common/proprietary/vendor/firmware/setfile_imx258.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/setfile_imx258.bin \
+    vendor/samsung/a53x-common/proprietary/vendor/firmware/setfile_imx616.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/setfile_imx616.bin \
+    vendor/samsung/a53x-common/proprietary/vendor/firmware/setfile_imx682.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/setfile_imx682.bin \
+    vendor/samsung/a53x-common/proprietary/vendor/firmware/swcnr_IMX682.json:$(TARGET_COPY_OUT_VENDOR)/firmware/swcnr_IMX682.json \
+    vendor/samsung/a53x-common/proprietary/vendor/firmware/swcnr_S5KGW1.json:$(TARGET_COPY_OUT_VENDOR)/firmware/swcnr_S5KGW1.json \
     vendor/samsung/a53x-common/proprietary/vendor/firmware/tfa9878.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/tfa9878.bin \
     vendor/samsung/a53x-common/proprietary/vendor/firmware/tfa98xx.cnt:$(TARGET_COPY_OUT_VENDOR)/firmware/tfa98xx.cnt \
     vendor/samsung/a53x-common/proprietary/vendor/firmware/txse1.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/txse1.bin \
     vendor/samsung/a53x-common/proprietary/vendor/firmware/txse2.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/txse2.bin \
     vendor/samsung/a53x-common/proprietary/vendor/firmware/volumemonitor.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/volumemonitor.bin \
+    vendor/samsung/a53x-common/proprietary/vendor/firmware/vts.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/vts.bin \
     vendor/samsung/a53x-common/proprietary/vendor_ramdisk/vendor/firmware/gt9895_a53x.bin:$(TARGET_COPY_OUT_VENDOR_RAMDISK)/vendor/firmware/gt9895_a53x.bin
 
 PRODUCT_PACKAGES += \
